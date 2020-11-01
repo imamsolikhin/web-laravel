40c9ca67da240e56fac8fba4c97afddcf5923be1
imamsolikhin <imamsolikhin@github.com>
2020-11-01 20:27:34 +0700

    Update _content.blade.php

--- private/resources/views/layout/base/_content.blade.php
+++ private/resources/views/layout/base/_content.blade.php
@@ -2,9 +2,11 @@
 @if (config('layout.content.extended'))
     @yield('content')
 @else
-    <div class="d-flex flex-column-fluid container-fluid">
+    <div class="d-flex flex-column-fluid">
         <!-- <div class="{{ Metronic::printClasses('content-container', false) }}"> -->
-            @yield('content')
+            <div class="container-fluid">
+                @yield('content')
+            </div>
         <!-- </div> -->
     </div>
 @endif
