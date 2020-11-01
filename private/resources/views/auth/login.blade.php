1ae70a05f9b09813218731a7ace22e58579028eb
imamsolikhin <imamsolikhin@github.com>
2020-11-01 19:42:41 +0700

    Update login.blade.php

--- private/resources/views/auth/login.blade.php
+++ private/resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
 @section('content')
   <div class="d-flex flex-column flex-root">
       <div class="login login-1 login-signin-on d-flex flex-column flex-lg-row flex-row-fluid bg-white" id="kt_login">
-          <div class="login-aside d-flex flex-row-auto bgi-size-cover bgi-no-repeat p-10 p-lg-10" style="background-image: {{ config('app.url') }}media/logos/logo-letter-8.png">
+          <div class="login-aside d-flex flex-row-auto bgi-size-cover bgi-no-repeat p-10 p-lg-40" style="background-image:url(https://image.freepik.com/free-vector/isometric-laptop-background-template_52683-7079.jpg )">
               <div class="d-flex flex-row-fluid flex-column justify-content-between">
                   <div class="flex-column-fluid d-flex flex-column justify-content-center">
-                      <h3 class="font-size-h1 mb-5 text-dark">HPI MANAGEMENT</h3>
-                      <p class="font-weight-lighter text-dark opacity-80">-.</p>
+                      <h3 class="font-size-h1 mb-5 text-white">HPI MANAGEMENT</h3>
+                      <p class="font-weight-lighter text-white opacity-80">-.</p>
                   </div>
                   <div class="d-none flex-column-auto d-lg-flex justify-content-between mt-10">
-                      <div class="opacity-70 font-weight-bold text-dark">Powered by NUIS 2020</div>
+                      <div class="opacity-70 font-weight-bold text-white">Powered by NUIS 2020</div>
                   </div>
               </div>
           </div>
@@ -23,7 +23,7 @@
                   <div class="login-form login-signin">
                       <div class="text-center mb-0 mb-lg-5">
                         <h3 class="font-size-h1"><b>LOGIN</b></h3>
-                        <!-- <h3 class="font-size-h1 mb-5 text-dark">HPI MANAGEMENT</h3>
+                        <!-- <h3 class="font-size-h1 mb-5 text-white">HPI MANAGEMENT</h3>
                           <a href="#" class="flex-column-auto mt-5">
                               <img src="#" class="max-h-70px" alt="" />
                           </a> -->
@@ -40,10 +40,10 @@
                           @include('inc.success-notif')
 
                           <div class="form-group">
-                              <input class="form-control h-auto text-dark placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5" type="text" placeholder="Email" name="login" value="{{ old('login') }}" required autocomplete="off"/>
+                              <input class="form-control h-auto text-white placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5" type="text" placeholder="Email" name="login" value="{{ old('login') }}" required autocomplete="off"/>
                           </div>
                           <div class="form-group">
-                              <input class="form-control h-auto text-dark placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5" type="password" placeholder="Password" name="password" required/>
+                              <input class="form-control h-auto text-white placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5" type="password" placeholder="Password" name="password" required/>
                           </div>
                           <div class="form-group d-flex flex-wrap justify-content-between align-items-center">
                               <div class="col-12 row">
@@ -77,7 +77,7 @@
                   </div>
               </div>
               <div class="d-flex d-lg-none flex-column-auto flex-column flex-sm-row justify-content-between align-items-center mt-5 p-5">
-                  <div class="text-dark-50 font-weight-bold order-2 order-sm-1 my-2">� 2020 TEAM&nbsp;&nbsp;R&D</div>
+                  <div class="text-white-50 font-weight-bold order-2 order-sm-1 my-2">� 2020 TEAM&nbsp;&nbsp;R&D</div>
               </div>
           </div>
       </div>
